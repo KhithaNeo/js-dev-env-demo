b9a0fc3a661a2c7ae1e1829fe469474d02a387c3
KhithaNeo <neokhitha@yahoo.com>
2019-07-04 11:48:21 -0700

    created buildScripts/srcServer.js folder and added code

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
